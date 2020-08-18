603b1ec631e09715392fa9c0d21d863be019442b
maxirmx <maxim@samsonov.net>
2020-08-18 19:21:23 +0300

    build.cmd 2

--- local/test.cmd
+++ local/test.cmd
@@ -2,7 +2,6 @@
 rem --------------------------------------------------------------------
 rem Test script to validate some steps of the build procedures locally
 rem Usage test.cmd <platform> <configuration>
-rem        platform: either 'amd64' or 'X64' or 'x86'
 rem        configuration: either 'debug' or 'release'
 rem --------------------------------------------------------------------
 
@@ -17,7 +16,7 @@ SET libjpeg_files="http://ijg.org/files"
 IF NOT EXIST libjpeg.zip powershell -ExecutionPolicy Bypass Invoke-WebRequest -Uri %Libjpeg_files%/jpegsr%libjpeg_version%.zip -ErrorAction Stop -OutFile libjpeg.zip
 "C:\Program Files\7-Zip\7z" -tzip -aoa x libjpeg.zip
 
-rem cmd /c build.cmd %1 %2
+cmd /c build.cmd %1 %libjpeg_version%
 
 cd local
 
